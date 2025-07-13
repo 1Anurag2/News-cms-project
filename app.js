bb32d0fc1cc2443fe4bbf6f940b92c389413044e
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-13 22:42:57 +0530

    Final commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const expressEjsLayouts = require("express-ejs-layouts");
 const cookieParser = require("cookie-parser");
 const connectdb = require("./config/connectdb");
+const recentNewsMiddleware = require('./middleware/recentnews');
+const setPaginationDefaults = require('./middleware/setPaginationDefaults');
 const path = require("path");
 require("dotenv").config();
 const app = express();
@@ -15,6 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(expressEjsLayouts);
 app.set("layout", "layout");
+
+// Use the recent news middleware globally
+app.use(recentNewsMiddleware);
+app.use(setPaginationDefaults);
+
 
 //view Engine
 app.set("view engine", "ejs");
