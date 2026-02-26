08b066fdfa2c58775473eb28032849be79574653
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2026-02-26 21:08:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const expressEjsLayouts = require("express-ejs-layouts");
-const cookieParser = require("cookie-parser");
-const connectdb = require("./config/connectdb");
-const recentNewsMiddleware = require('./middleware/recentnews');
-const setPaginationDefaults = require('./middleware/setPaginationDefaults');
-const path = require("path");
-require("dotenv").config();
-const app = express();
+// const express = require("express");
+// const expressEjsLayouts = require("express-ejs-layouts");
+// const cookieParser = require("cookie-parser");
+// const connectdb = require("./config/connectdb");
+// const recentNewsMiddleware = require('./middleware/recentnews');
+// const setPaginationDefaults = require('./middleware/setPaginationDefaults');
+// const path = require("path");
+// require("dotenv").config();
+// const app = express();
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
-//middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(expressEjsLayouts);
-app.set("layout", "layout");
+// //middleware
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(expressEjsLayouts);
+// app.set("layout", "layout");
 
-// Use the recent news middleware globally
-app.use(recentNewsMiddleware);
-app.use(setPaginationDefaults);
+// // Use the recent news middleware globally
+// app.use(recentNewsMiddleware);
+// app.use(setPaginationDefaults);
 
 
-//view Engine
-app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
+// //view Engine
+// app.set("view engine", "ejs");
+// // app.set("views", path.join(__dirname, "views"));
 
-//Database Connection
-connectdb();
+// //Database Connection
+// connectdb();
 
-//Routes
-app.use("/", require("./routes/frontend"));
+// //Routes
+// app.use("/", require("./routes/frontend"));
 
-app.use("/admin", (req, res, next) => {
-  res.locals.layout = 'admin/layout';
-  next();
-});
-app.use("/admin", require("./routes/admin"));
+// app.use("/admin", (req, res, next) => {
+//   res.locals.layout = 'admin/layout';
+//   next();
+// });
+// app.use("/admin", require("./routes/admin"));
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running at ${port}`);
-});
+// app.listen(port, (req, res) => {
+//   console.log(`Server is running at ${port}`);
+// });
